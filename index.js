8da610215a439eea91b653e3c5a04a2734dc5a14
sona.rohal <srohalova@gmail.com>
2025-03-02 12:39:58 +0100

    adj + more than 36

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const n = Number(prompt('Zadajte číslo rulety: 0-36.')); 
 
-if (n  === 0) {
+if ( n > 36 ) {
+  document.body.innerHTML += '<p>Číslo nie je zadané správne.</p>'; 
+}
+
+else if (n  === 0) {
   document.body.innerHTML += '<p>Číslo nie je ani liché ani sudé a zároveň ani červené ani čierne.</p>'; 
 
-
 } else if (n % 2 === 0) { 
-  document.body.innerHTML += '<p>Číslo je sudé.</p>';
+  document.body.innerHTML += '<p>Zadané číslo je sudé.</p>';
   if (n >=1 && n <=10 || n >=19 && n <=28 ) {
-    document.body.innerHTML += '<p>Číslo je černé.</p>';
+    document.body.innerHTML += '<p>Zadané číslo je černé.</p>';
   } else {
-    document.body.innerHTML += '<p>Číslo je červené.</p>';
+    document.body.innerHTML += '<p>Zadané číslo je červené.</p>';
   }
 
 } else {
-  document.body.innerHTML += '<p>Číslo je liché.</p>'; 
-  if (n >=10 && n <=19 || n >=28 ) {
-    document.body.innerHTML += '<p>Číslo je červené.</p>';
+  document.body.innerHTML += '<p>Zadané číslo je liché.</p>'; 
+  if (n > 10 && n <19 || n >28 ) {
+    document.body.innerHTML += '<p>Zadané číslo je černé.</p>';
   } else {
-    document.body.innerHTML += '<p>Číslo je černé.</p>';
+    document.body.innerHTML += '<p>Zadané číslo je červené.</p>';
   }
 }
